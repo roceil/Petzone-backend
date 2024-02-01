f560e0241470b5ff0b7d59568618701c0c2aac5c
ycl818 <kevinyichienlee@gmail.com>
2024-02-01 15:22:32 +0800

    add petzone production allowedOrigins

--- src/configs/allowedOrigins.js
+++ src/configs/allowedOrigins.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://127.0.0.1:3030",
   "http://localhost:3030",
+  "https://petzone-backend.zeabur.app",
 ];
 
 module.exports = allowedOrigins;
