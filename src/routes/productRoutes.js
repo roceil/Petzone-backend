fb052158f5ea1770fa7b73a0cac6d781d08238f5
MSI\user <s97011501@gmail.com>
2024-03-04 15:24:17 +0800

    feat(postroutes): 新增上傳多張圖片 api

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,22 +1,27 @@
-const express = require("express");
-const router = express.Router();
-const productController = require("../controllers/productController");
-const uploadController = require("../controllers/uploadController");
+const express = require('express')
+const router = express.Router()
+const productController = require('../controllers/productController')
+const uploadController = require('../controllers/uploadController')
 
 // 前台產品相關
-router.get("/products", productController.userGetProducts);
-router.get("/product/:productId", productController.userGetProduct);
+router.get('/products', productController.userGetProducts)
+router.get('/product/:productId', productController.userGetProduct)
 
 // 後台產品相關
-router.post("/product", productController.addProduct);
-router.delete("/product/:productId", productController.deleteProduct);
-router.put("/product/:productId", productController.updateProduct);
-router.get("/products/admin", productController.getProducts);
-router.get("/product/admin/:productId", productController.getProduct);
+router.post('/product', productController.addProduct)
+router.delete('/product/:productId', productController.deleteProduct)
+router.put('/product/:productId', productController.updateProduct)
+router.get('/products/admin', productController.getProducts)
+router.get('/product/admin/:productId', productController.getProduct)
 router.post(
-  "/upload/image",
-  uploadController.upload.single("file"),
+  '/upload/image',
+  uploadController.upload.single('file'),
   uploadController.uploadImage
-);
+)
+router.post(
+  '/upload/images',
+  uploadController.upload.array('files', 3),
+  uploadController.uploadImages
+)
 
-module.exports = router;
+module.exports = router
