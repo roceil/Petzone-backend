0a2c2013ad8bdc5b5e9bcdfb0341273c04622359
roceil <a0978006326@gmail.com>
2024-03-05 23:04:07 +0800

    feat(ordercontroller): 完成依userId取訂單

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -1,4 +1,5 @@
 const Order = require('../models/order-model')
+const { getTokenInfo } = require('../lib')
 
 function generateRandomNumber() {
   return Math.floor(100000 + Math.random() * 900000)
@@ -57,8 +58,32 @@ const getAllOrders = async (req, res) => {
   }
 }
 
+const getOrderByUserId = async (req, res) => {
+  const authCheck = await getTokenInfo(req)
+  if (!authCheck) {
+    return res.status(401).json({ message: 'Not authorized' })
+  }
+
+  const userId = req.query.userId
+  try {
+    const orders = await Order.find({ userId: userId })
+    if (!orders.length) {
+      return res.status(202).json({ message: '尚無訂單' })
+    }
+
+    res.json({
+      status: 'success',
+      totalOrders: orders.length,
+      orders,
+    })
+  } catch (error) {
+    res.status(500).json({ message: '請檢查API格式或參數是否有誤' })
+  }
+}
+
 module.exports = {
   createOrder,
   getAllOrders,
   getOrderByOrderId,
+  getOrderByUserId,
 }
