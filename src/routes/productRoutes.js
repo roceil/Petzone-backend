89d57283bdd7ad0720cf3f2a32a0608ef9113ae5
HsuXinYu <jolin7180732@gmail.com>
2024-04-10 17:55:44 +0800

    feat(productroutes): 增加 isAdmin

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const productController = require('../controllers/productController')
 const productReviewController = require('../controllers/productReviewController')
 const uploadController = require('../controllers/uploadController')
-const { isUser } = require('../middleware/auth')
+const { isUser, isAdmin } = require('../middleware/auth')
 
 // 前台取得全部產品資訊
 router.get('/products', productController.userGetProducts)
@@ -39,23 +39,24 @@ router.delete(
 )
 
 // 後台取得全部產品資訊
-router.get('/products/admin', productController.getProducts)
+router.get('/products/admin', isAdmin, productController.getProducts)
 
 // 後台取得單一產品資訊
-router.get('/product/admin/:productId', productController.getProduct)
+router.get('/product/admin/:productId', isAdmin, productController.getProduct)
 
 // 後台新增產品
-router.post('/product', productController.addProduct)
+router.post('/product', isAdmin, productController.addProduct)
 
 // 後台更新產品
-router.put('/product/:productId', productController.updateProduct)
+router.put('/product/:productId', isAdmin, productController.updateProduct)
 
 // 後台刪除產品
-router.delete('/product/:productId', productController.deleteProduct)
+router.delete('/product/:productId', isAdmin, productController.deleteProduct)
 
 // 後台刪除商品評價
 router.delete(
   '/product/admin/review/:productId/:userId',
+  isAdmin,
   productReviewController.deleteProductReview
 )
 
